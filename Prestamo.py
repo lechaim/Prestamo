95c20e9efad63705d6495e8c4a29bcd5760300b5
lechaim <lechaimz18@gmail.com>
2019-05-15 12:12:30 -0400

    probando

---
+++ Prestamo.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+class Prestamo:
+	def __init__(self, root):
+		self.root = root
+		root.title("Simple GUI")
+
+		self.lblCantidad = Label(root, text="Cantidad")
+		self.lblCantidad.pack()
+
+		self.lblInteres = Label(root, text="Interés")
+		self.lblInteres.pack()
+
+		self.lblTipoPago = Label(root, text="Tipo de pago")
+		self.lblTipoPago.pack()
+
+		self.lblTiempo = Label(root, text="Tiempo de pago")
+		self.lblTiempo.pack()
+        
+
+
+root = Tk()
+my_gui = Prestamo(root)
+root.mainloop()
