be464c66b1aa5f6bf57120ac7796fb4f65f19e0f
lechaim <lechaimz18@gmail.com>
2019-05-16 10:07:51 -0400

    Se agregaron labels

--- Prestamo.py
+++ Prestamo.py
@@ -1,24 +1,32 @@
 from tkinter import *
+from tkinter import ttk
 
 class Prestamo:
-	def __init__(self, root):
-		self.root = root
-		root.title("Simple GUI")
+    def __init__(self, root):
+        self.root = root
+        root.title("Simple GUI")
+        root.geometry("550x650")
 
-		self.lblCantidad = Label(root, text="Cantidad")
-		self.lblCantidad.pack()
+        #labels
+        self.lblCantidad = Label(root, text="Cantidad").grid(row=0, column=0)       
 
-		self.lblInteres = Label(root, text="Interés")
-		self.lblInteres.pack()
+        self.lblInteres = Label(root, text="Interés").grid(row=1, column=0)
 
-		self.lblTipoPago = Label(root, text="Tipo de pago")
-		self.lblTipoPago.pack()
+        self.lblTipoPago = Label(root, text="Tipo de pago").grid(row=2, column=0)
 
-		self.lblTiempo = Label(root, text="Tiempo de pago")
-		self.lblTiempo.pack()
-        
-        #comentario de prueba
-        sasa
+        self.lblTiempo = Label(root, text="Tiempo de pago").grid(row=3, column=0)
+
+        # textboxes
+
+        self.txtCantidad = Entry(root).grid(row=0, column=1)
+
+        self.txtInteres =  Entry(root).grid(row=1, column=1)
+
+        self.cmbTipoPago = ttk.Combobox(root, values = ["Semanal", "Quincenal", "Mensual"], width = 18).grid(row=2, column=1)
+
+        self.txtTiempo = Entry(root).grid(row=3, column=1)
+        #ubuntu
+
 
 
 root = Tk()
