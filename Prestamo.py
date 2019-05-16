93a142e3a8c88debface2a33df8a41af632de445
lechaim <lechaimz18@gmail.com>
2019-05-16 15:42:59 -0400

    se agregaron los metodos

--- Prestamo.py
+++ Prestamo.py
@@ -5,28 +5,58 @@ class Prestamo:
     def __init__(self, root):
         self.root = root
         root.title("Simple GUI")
-        root.geometry("550x650")
+        root.geometry("350x300")
 
         #labels
-        self.lblCantidad = Label(root, text="Cantidad").grid(row=0, column=0)       
+        self.lblCantidad = Label(root, text="Cantidad")
+        self.lblCantidad.grid(row=0, column=0, pady = 10, padx = 5)       
 
-        self.lblInteres = Label(root, text="Interés").grid(row=1, column=0)
+        self.lblInteres = Label(root, text="Interés")
+        self.lblInteres.grid(row=1, column=0, pady = 10, padx = 5)
 
-        self.lblTipoPago = Label(root, text="Tipo de pago").grid(row=2, column=0)
+        self.lblTipoPago = Label(root, text="Tipo de pago")
+        self.lblTipoPago.grid(row=2, column=0, pady = 10, padx = 5)
 
-        self.lblTiempo = Label(root, text="Tiempo de pago").grid(row=3, column=0)
+        self.lblTiempo = Label(root, text="Tiempo de pago")
+        self.lblTiempo.grid(row=3, column=0, pady = 10, padx = 15)
 
         # textboxes
 
-        self.txtCantidad = Entry(root).grid(row=0, column=1)
+        self.txtCantidad = Entry(root)
+        self.txtCantidad.grid(row=0, column=1, pady = 10, padx = 5)
 
-        self.txtInteres =  Entry(root).grid(row=1, column=1)
+        self.txtInteres =  Entry(root)
+        self.txtInteres.grid(row=1, column=1, pady = 10, padx = 5)
 
-        self.cmbTipoPago = ttk.Combobox(root, values = ["Semanal", "Quincenal", "Mensual"], width = 18).grid(row=2, column=1)
+        self.cmbTipoPago = ttk.Combobox(root, values = ["Semanal", "Quincenal", "Mensual"], width = 17)
+        self.cmbTipoPago.grid(row=2, column=1, pady = 10, padx = 5)
 
-        self.txtTiempo = Entry(root).grid(row=3, column=1)
-        #ubuntu
+        self.txtTiempo = Entry(root)
+        self.txtTiempo.grid(row=3, column=1, pady = 10, padx = 5)
+        
+        #botones
 
+        self.btnCalcular = Button(root, text = "Calcular", command = self.calcular).grid(row=4, column=0, pady = 10, padx = 5)
+        self.btnLimpiar = Button(root, text = "Limpiar", ).grid(row=4, column=1, pady = 10, padx = 5)
+
+
+        #funciones
+
+        interes = self.txtInteres.get()
+        cantidad = self.txtCantidad.get()
+
+
+    def borrar(self):
+    	self.txtTiempo.delete(0, "END")
+    	self.txtTiempo.update()
+
+    def calcular(self):
+    	newWindow = Toplevel(root)
+    	newWindow.title("Pagos a realizar")
+    	newWindow.geometry("350x300")
+
+    	self.calculo = Label(newWindow, text="Aqui")
+    	self.calculo.grid(row=0, column=0, pady = 10, padx = 5)
 
 
 root = Tk()
